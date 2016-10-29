0e979253f1a232e7a711aaf1188563b5184a88c5
Manu Phatak <manu@thinkcerca.com>
2016-10-29 12:18:32 -0500

    Setup routing logic

--- src/Main.elm
+++ src/Main.elm
@@ -1,16 +1,22 @@
 module Main exposing (..)
 
-import Html.App
+import Navigation
 import Messages exposing (Msg(..))
 import Models exposing (Model, initialModel)
 import View exposing (view)
 import Update exposing (update)
 import Players.Commands exposing (fetchAll)
+import Routing exposing (Route)
 
 
-init : ( Model, Cmd Msg )
-init =
-    ( initialModel, Cmd.map PlayersMsg fetchAll )
+
+init : Result String Route -> ( Model, Cmd Msg )
+init result =
+    let
+        currentRoute =
+            Routing.routeFromResult result
+    in
+        ( initialModel currentRoute, Cmd.map PlayersMsg fetchAll )
 
 
 
@@ -21,16 +27,25 @@ subscriptions : Model -> Sub Msg
 subscriptions model =
     Sub.none
 
+-- URL UPDATE
 
+urlUpdate : Result String Route -> Model -> ( Model, Cmd Msg)
+urlUpdate result model =
+    let
+        currentRoute =
+            Routing.routeFromResult result
+    in
+        ({ model | route = currentRoute }, Cmd.none)
 
 -- MAIN
 
 
 main : Program Never
 main =
-    Html.App.program
+    Navigation.program Routing.parser
         { init = init
         , view = view
         , update = update
+        , urlUpdate = urlUpdate
         , subscriptions = subscriptions
         }
--- src/View.elm
+++ src/View.elm
@@ -5,6 +5,9 @@ import Html.App
 import Messages exposing (Msg(..))
 import Models exposing (Model)
 import Players.List
+import Players.Edit
+import Players.Models exposing (PlayerId)
+import Routing exposing (Route(..))
 
 
 view : Model -> Html Msg
@@ -15,4 +18,34 @@ view model =
 
 page : Model -> Html Msg
 page model =
-    Html.App.map PlayersMsg (Players.List.view model.players)
+    case model.route of
+        PlayersRoute ->
+            Html.App.map PlayersMsg (Players.List.view model.players)
+
+        PlayerRoute id ->
+            playerEditPage model id
+
+        NotFoundRoute ->
+            notFoundView
+
+
+playerEditPage : Model -> PlayerId -> Html Msg
+playerEditPage model playerId =
+    let
+        maybePlayer =
+            model.players
+                |> List.filter (\player -> player.id == player.id)
+                |> List.head
+    in
+        case maybePlayer of
+            Just player ->
+                Html.App.map PlayersMsg (Players.Edit.view player)
+
+            Nothing ->
+                notFoundView
+
+
+notFoundView : Html Msg
+notFoundView =
+    div []
+        [ text "Not found" ]
