0d0388f6a577ba769ef05abbcfa8ecdaa26873d0
Manu Phatak <manu@thinkcerca.com>
2017-06-18 00:28:26 -0500

    Starter app

--- src/Main.elm
+++ src/Main.elm
@@ -1,93 +1,67 @@
 module Main exposing (..)
 
-import Html exposing (..)
-import Html.Attributes exposing (..)
-import Html.Events exposing (onClick)
-
-
--- component import example
-
-import Components.Hello exposing (hello)
-
-
--- APP
-
-
-main : Program Never Int Msg
-main =
-    Html.beginnerProgram { model = model, view = view, update = update }
-
+import Html exposing (div, program, Html, text)
 
 
 -- MODEL
 
 
 type alias Model =
-    Int
+    String
 
 
-model : number
-model =
-    0
+init : ( Model, Cmd Msg )
+init =
+    ( "Hello", Cmd.none )
+
+
+
+-- MESSAGES
+
+
+type Msg
+    = NoOp
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ text model ]
 
 
 
 -- UPDATE
 
 
-type Msg
-    = NoOp
-    | Increment
-
-
-update : Msg -> Model -> Model
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         NoOp ->
-            model
-
-        Increment ->
-            model + 1
+            ( model, Cmd.none )
 
 
 
--- VIEW
--- Html is defined as: elem [ attribs ][ children ]
--- CSS can be applied via class names or inline style attrib
+-- SUBSCRIPTIONS
 
 
-view : Model -> Html Msg
-view model =
-    div [ class "container", style [ ( "margin-top", "30px" ), ( "text-align", "center" ) ] ]
-        [ -- inline CSS (literal)
-          div [ class "row" ]
-            [ div [ class "col-xs-12" ]
-                [ div [ class "jumbotron" ]
-                    [ img [ src "public/img/elm.jpg", style styles.img ] []
-                      -- inline CSS (via var)
-                    , hello model
-                      -- ext 'hello' component (takes 'model' as arg)
-                    , p [] [ text ("Elm Webpack Starter") ]
-                    , button [ class "btn btn-primary btn-lg", onClick Increment ]
-                        [ -- click handler
-                          span [ class "glyphicon glyphicon-star" ] []
-                          -- glyphicon
-                        , span [] [ text "FTW!" ]
-                        ]
-                    ]
-                ]
-            ]
-        ]
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
 
 
 
--- CSS STYLES
+-- MAIN
 
 
-styles : { img : List ( String, String ) }
-styles =
-    { img =
-        [ ( "width", "33%" )
-        , ( "border", "4px solid #337AB7" )
-        ]
-    }
+main : Program Never Model Msg
+main =
+    program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
